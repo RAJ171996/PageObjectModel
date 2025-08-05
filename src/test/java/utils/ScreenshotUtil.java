ad467bb4553903bd2a33e1a076c0ff2b2a08ab0e
Raj Kumar <r.rajkumar1514@gmail.com>
2025-08-05 17:24:30 +0530

    Updated ci.yml file

--- src/test/java/utils/ScreenshotUtil.java
+++ src/test/java/utils/ScreenshotUtil.java
@@ -1,8 +1,5 @@
 package utils;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,26 +7,29 @@ import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 public class ScreenshotUtil {
-    public static String captureScreenshot(WebDriver driver, String methodName) {
-        String timestamp = new SimpleDateFormat("dd-MMM-yyyy hh.mm.ss a").format(new Date());
-        String dirPath = "screenshots";
-        String fileName = methodName + "-" + timestamp + ".png";
+	public static String captureScreenshot(WebDriver driver, String methodName) {
+		String timestamp = new SimpleDateFormat("dd-MMM-yyyy hh.mm.ss a").format(new Date());
+		String dirPath = "screenshots";
+		String fileName = methodName + "-" + timestamp + ".png";
 
-        File screenshotsDir = new File(dirPath);
-        if (!screenshotsDir.exists()) {
-            screenshotsDir.mkdirs();
-        }
+		File screenshotsDir = new File(dirPath);
+		if (!screenshotsDir.exists()) {
+			screenshotsDir.mkdirs();
+		}
 
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        File dest = new File(screenshotsDir, fileName);
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File dest = new File(screenshotsDir, fileName);
 
-        try {
-            Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return dest.getAbsolutePath();
-    }
+		try {
+			Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "screenshots/" + fileName;
+	}
 }
