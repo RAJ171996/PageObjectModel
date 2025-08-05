bb40c264c08fb0aa54c183ae3d5bd00c9bc22c3d
Raj Kumar <r.rajkumar1514@gmail.com>
2025-08-05 19:16:35 +0530

    Updated ci.yml file

--- src/test/java/utils/ScreenshotUtil.java
+++ src/test/java/utils/ScreenshotUtil.java
@@ -2,36 +2,26 @@ package utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class ScreenshotUtil {
-	public static String captureScreenshot(WebDriver driver, String methodName) {
-		String timestamp = new SimpleDateFormat("dd-MMM-yyyy-hh.mm.ss-a").format(new Date());
-		String dirPath = "screenshots";
-		String fileName = methodName + "-" + timestamp + ".png";
-
-		File screenshotsDir = new File(dirPath);
-		if (!screenshotsDir.exists()) {
-			screenshotsDir.mkdirs();
-		}
-
+	public static String captureScreenshot(WebDriver driver, String methodName) throws IOException {
 		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String timestamp = new SimpleDateFormat("dd-MMM-yyyy hh.mm.ss a").format(new Date());
+		File screenshotsDir = new File("screenshots"); // relative folder
+		if (!screenshotsDir.exists())
+			screenshotsDir.mkdirs();
+
+		String fileName = methodName + "_" + timestamp + ".png";
 		File dest = new File(screenshotsDir, fileName);
+		FileUtils.copyFile(src, dest);
 
-		try {
-			Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
-			System.out.println("📸 Screenshot saved at: " + dest.getAbsolutePath());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return dest.getAbsolutePath();
-
+		return "screenshots/" + fileName;
 	}
 }
